53a0bf19cf08ca16a2b03416d8b54b95dbcfbd68
capitalgoosega <capitalgoose.ga@gmail.com>
2026-06-28 16:38:41 -0400

    Update webhooks.py

--- app/api/webhooks.py
+++ app/api/webhooks.py
@@ -1,19 +1,18 @@
-from fastapi import APIRouter, Request, HTTPException
+from fastapi import APIRouter, Request, HTTPException, Depends
+from sqlalchemy.orm import Session
 from app.db.session import get_db
 from app.models.application import Application
 from app.services.workflow_service import process_intake
 from app.schemas.intake import IntakeRequest
-from sqlalchemy.orm import Session
-from fastapi import Depends
 
 router = APIRouter()
 
 @router.post("/cognito")
 async def cognito_webhook(request: Request, db: Session = Depends(get_db)):
-    
+
     raw = await request.json()
     print("RAW PAYLOAD:", raw)
-    
+
     try:
         intake_data = IntakeRequest(
             first_name=raw["Entry.FirstName"],
@@ -21,9 +20,8 @@ async def cognito_webhook(request: Request, db: Session = Depends(get_db)):
             email=raw["Entry.EmailAddress"],
             loan_amount=int(raw["Entry.LoanAmount"]),
             loan_type=raw["Entry.LoanType"],
-            ssn=raw["Entry.SSN"],
             dob=raw["Entry.DateOfBirth"],
-            address= {
+            address={
                 "street": raw["Entry.Street"],
                 "city": raw["Entry.City"],
                 "state": raw["Entry.State"],
@@ -31,20 +29,20 @@ async def cognito_webhook(request: Request, db: Session = Depends(get_db)):
             }
         )
     except KeyError as e:
+        print("RAW PAYLOAD DUMP:", raw)
         raise HTTPException(status_code=400, detail=f"Missing field from Cognito: {e}")
-    
-    score, status = process_intake(db, intake_data)
-    
+
+    status = process_intake(db, intake_data)
+
     app_record = Application(
-        name=intake_data.name,
+        name=f"{intake_data.first_name} {intake_data.last_name}",
         email=intake_data.email,
         loan_amount=intake_data.loan_amount,
         loan_type=intake_data.loan_type,
-        credit_score=score,
         status=status
     )
-    
+
     db.add(app_record)
     db.commit()
-    
+
     return {"received": True, "status": status}
