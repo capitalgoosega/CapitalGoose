d134d3fcf7317498a480198002811f5136a6fcf5
capitalgoosega <capitalgoose.ga@gmail.com>
2026-08-08 13:05:32 -0400

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -6,6 +6,9 @@ class Settings(BaseSettings):
     gmail_app_password: str = ""
     sender_email: str = ""
     resend_api_key: str = ""
+    gmail_client_id: str = ""
+    gmail_client_secret: str = ""
+    gmail_refresh_token: str = ""
 
     class Config:
         env_file = ".env"
