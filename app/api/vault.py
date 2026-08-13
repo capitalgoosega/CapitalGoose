679c47f68f85a77933deb35fd1f8f309dcdd22db
capitalgoosega <capitalgoose.ga@gmail.com>
2026-08-13 07:30:21 -0400

    Update vault.py

--- app/api/vault.py
+++ app/api/vault.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
 from fastapi import APIRouter, Depends, HTTPException
+# deploy
 from fastapi.responses import HTMLResponse, Response
 from pydantic import BaseModel, EmailStr
 from sqlalchemy.orm import Session
